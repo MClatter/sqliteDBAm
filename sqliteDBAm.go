a0e040194138a35c6c3fa502b1867a8eda68a47c
Михаил Тарабанько <mikeclatter@gmail.com>
2019-11-07 17:23:07 +0300

    Add query for fetching all suited notes

--- sqliteDBAm.go
+++ sqliteDBAm.go
@@ -1,7 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+type note struct {
+	id   int
+	flds string
+}
 
 func main() {
-	fmt.Println("Working so far")
+	db, err := sql.Open("sqlite3", "C:/Users/Michael/Documents/SQLite scripts/collection.anki2")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	rows, err := db.Query("SELECT id, flds FROM notes WHERE flds LIKE '%%' AND tags LIKE '%SMIntermediate%'")
+	if err != nil {
+		panic(err)
+	}
+	defer rows.Close()
+
+	noteSlice := []note{}
+	for rows.Next() {
+		noteVar := note{}
+		err := rows.Scan(&noteVar.id, &noteVar.flds)
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+		noteSlice = append(noteSlice, noteVar)
+	}
+
+	fmt.Println(len(noteSlice))
 }
